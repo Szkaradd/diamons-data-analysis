b4b44ddcbb2667246149b0a698bf7f3258ef69ba
Szkaradd <szkaradekmikolaj@gmail.com>
2024-02-03 23:05:48 +0100

    add fully functional dashboard

--- dashboard.py
+++ dashboard.py
@@ -3,23 +3,76 @@ import pandas as pd
 import numpy as np
 from PIL import Image
 import datetime
+import joblib
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+clean_data = pd.read_csv("clean_data.csv")
+continuous_vars = ['carat', 'length', 'width', 'height', 'depth', 'table', 'price']
+categorical_vars = ['cut', 'color', 'clarity']
+
+def plot_histplot(data, col):
+    plt.figure(figsize=(10, 4))
+    sns.histplot(data [col], bins=20)
+    plt.title(f'Distribution of {col}')
+    st.pyplot(plt)
+
+def plot_piechart(data, col):
+    plt.figure(figsize=(10, 4))
+    data[col].value_counts().plot.pie(autopct='%1.1f%%')
+    plt.title(f'Pie chart of {col}')
+    plt.axis('equal')
+    st.pyplot(plt)
+
+def regression_plot(y, y_pred):
+    fig, ax = plt.subplots(figsize=(10, 6))
+    plt.scatter(y, y_pred, color='blue')
+    ax.set_xlabel('Real prices')
+    ax.set_ylabel('Expected prices')
+    ax.set_title('Real vs Expected diamond prices')
+    plt.plot([min(y), max(y)], [min(y_pred), max(y_pred)], color='red')
+    st.pyplot(fig)
+    
+
+# Page Title
 st.title("Diamond data analysis")
 
-df = pd.read_csv("clean_data.csv")
+# Show data
+st.write(clean_data)
 
-st.write(df)
+st.header("Diamonds by category")
+category = st.selectbox("Select a category", categorical_vars)
 
-st.header("Diamonds by cut")
+plot_piechart(clean_data, category)
 
-cut = st.selectbox("Select a cut", df["cut"].unique())
+choice = st.selectbox(f'Select a {category}', clean_data[category].unique())
 
-st.write(df[df["cut"] == cut])
+st.write(clean_data[clean_data[category] == choice])
 
-occupation = st.selectbox("Your Occupation", ("Programmer", "Data Analyst", "Teacher"))
-st.write("You selected this occupation: ", occupation)
+# Continuous variables distribution
+st.header("Continuous variables distribution")
 
-data = st.file_uploader("Upload your dataset", type=['csv'])
-if data is not None:
-    df = pd.read_csv(data)
-    st.dataframe(df.head(10))
+col = st.selectbox("Select a variable", continuous_vars)
+plot_histplot(clean_data, col)
+
+# Model
+
+X = pd.read_csv("model_train_data.csv")
+y = pd.read_csv("model_expected_data.csv")
+y = y['price']
+
+model = joblib.load("model.joblib")
+
+# Plot linear regression of encoded data
+
+st.header("Linear regression of encoded data")
+
+y_pred = model.predict(X)
+
+show_score = st.checkbox("Show model score")
+if show_score:
+    st.write("Model score: ", model.score(X, y))
+
+show_plot = st.checkbox("Show regression plot")
+if show_plot:
+    regression_plot(y, y_pred)
